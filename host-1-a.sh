d8209524ff49c0dc7c11e186a5333085ca82d30f
aabriani <andreabriani@gmail.com>
2019-07-02 16:20:24 +0200

    fix ip address for eth1 host-1-a

--- host-1-a.sh
+++ host-1-a.sh
@@ -3,5 +3,5 @@ apt-get update
 apt-get install -y tcpdump --assume-yes
 
 ip link set eth1 up
-ip addr add 192.168.1.1/24 eth1
+ip addr add 192.168.1.1/24 dev eth1
 ip route add 192.168.2.96/27 via 192.168.1.254
