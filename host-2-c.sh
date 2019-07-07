fdd70c9e8f6809ab3595d8f324633afb948ae920
aabriani <andreabriani@gmail.com>
2019-07-07 19:17:29 +0200

    set host-2-c new interface name and added docker container script

--- host-2-c.sh
+++ host-2-c.sh
@@ -6,9 +6,26 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-get install -y docker-ce --assume-yes --force-yes
 
-ip link set dev eth1 up
+ip link set dev enp0s8 up
 
-ip addr add 172.16.1.1/30 dev eth1
+ip addr add 172.16.1.1/30 dev enp0s8
 
 ip route add 192.168.0.0/16 via 172.16.1.2
 ip route add 10.0.0.0/8 via 172.16.1.2
+
+docker kill $(docker ps -q)
+docker rm $(docker ps -aq)
+docker pull nginx
+mkdir -p ~/docker-webserver/html
+echo "<html>
+<head><title>DNCS Assignment A.Y. 2018/2019</title></head>
+<body>
+  <p>
+    <h1>
+      Page created just to test infrastructure worked properly.
+    </h1>
+  <p>
+</body>
+</html>" > ~/docker-webserver/html/index.html
+
+docker run --name docker-nginx -p 80:80 -d -v ~/docker-webserver/html:/usr/share/nginx/html nginx
